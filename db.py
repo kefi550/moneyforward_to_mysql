8c6d5184018c594aa38ac18f600eeccaa7424736
kefi550 <ytiryrstd550@gmail.com>
2024-11-29 17:33:51 +0900

    とりあえず

--- db.py
+++ db.py
@@ -1,16 +1,9 @@
-import os
 import pymysql
 from sqlalchemy import create_engine, String, Date, BigInteger
 from sqlalchemy.orm import mapped_column, Mapped, Session
 from datetime import date
 from sqlalchemy.ext.declarative import declarative_base
 
-
-DB_HOST = os.environ.get('DB_HOST', 'localhost')
-DB_USER = os.environ['DB_USER']
-DB_PASSWORD = os.environ['DB_PASSWORD']
-DB_PORT = int(os.environ.get('DB_PORT', 3306))
-DB_NAME = os.environ.get('DB_NAME', 'mf_kakeibo')
 
 Base = declarative_base()
 
@@ -19,12 +12,13 @@ class Cashflow(Base):
     __tablename__ = 'cashflow'
 
     id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=False)
+    group: Mapped[str] = mapped_column(String(100), nullable=False)
     calc: Mapped[bool] = mapped_column(nullable=False)
     date: Mapped[date] = mapped_column(Date, nullable=False)
     fiscal_year: Mapped[int] = mapped_column(nullable=False)
     fiscal_month: Mapped[int] = mapped_column(nullable=False)
     # 内容
-    content: Mapped[str] = mapped_column(String(100), nullable=False)
+    content: Mapped[str] = mapped_column(String(200), nullable=False)
     # 金額
     amount: Mapped[int] = mapped_column(nullable=False)
     # 金融機関
@@ -40,29 +34,54 @@ class Cashflow(Base):
 class Budget(Base):
     __tablename__ = 'budget'
 
+    group: Mapped[str] = mapped_column(String(30), primary_key=True)
     lcategory: Mapped[str] = mapped_column(String(30), primary_key=True)
-    budget: Mapped[int] = mapped_column(nullable=True)
+    fiscal_year: Mapped[int] = mapped_column(primary_key=True)
+    fiscal_month: Mapped[int] = mapped_column(primary_key=True)
+    budget: Mapped[int] = mapped_column(nullable=False)
 
 
-def create_database_if_not_exists():
+class ExpenseRatio(Base):
+    __tablename__ = 'expense_ratio'
+
+    fiscal_year: Mapped[int] = mapped_column(primary_key=True)
+    fiscal_month: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30), primary_key=True)
+    ratio: Mapped[float] = mapped_column(nullable=False)
+
+
+def create_database_if_not_exists(
+    host,
+    user,
+    password,
+    port,
+    db_name,
+):
     conn = pymysql.connect(
-        host=DB_HOST,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        port=DB_PORT,
+        host=host,
+        user=user,
+        password=password,
+        port=port,
         charset='utf8mb4'
     )
     try:
         with conn.cursor() as cursor:
-            cursor.execute(f'CREATE DATABASE IF NOT EXISTS {DB_NAME}')
+            cursor.execute(f'CREATE DATABASE IF NOT EXISTS {db_name}')
         conn.commit()
     finally:
         conn.close()
 
 
-create_database_if_not_exists()
-
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-engine = create_engine(DATABASE_URL, echo=True)
-Base.metadata.create_all(engine)
-session = Session(engine)
+def create_session(
+    host,
+    user,
+    password,
+    port,
+    db_name,
+):
+    create_database_if_not_exists(host, user, password, port, db_name)
+    DATABASE_URL = f"mysql+pymysql://{user}:{password}@{host}:{port}/{db_name}"
+    engine = create_engine(DATABASE_URL, echo=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    return session
--- upsert_cashflows.py
+++ upsert_cashflows.py
@@ -1,39 +1,73 @@
-import argparse
+import datetime
 import os
+from dateutil.relativedelta import relativedelta
 from moneyforward_selenium import MoneyForwardScraper
-from db import Cashflow, session
+from db import Cashflow, Budget, create_session
 
 
+UPDATE_MONTHS = 3  # 何ヶ月前までのデータを取得するか
 MONEYFORWARD_USER = os.environ["MONEYFORWARD_USER"]
 MONEYFORWARD_PASSWORD = os.environ["MONEYFORWARD_PASSWORD"]
+MONEYFORWARD_GROUP = os.environ["MONEYFORWARD_GROUP"]
+DB_HOST = os.environ.get('DB_HOST', 'localhost')
+DB_USER = os.environ['DB_USER']
+DB_PASSWORD = os.environ['DB_PASSWORD']
+DB_PORT = int(os.environ.get('DB_PORT', 3306))
+DB_NAME = os.environ.get('DB_NAME', 'mf_kakeibo')
+SELENIUM_HOST = os.environ.get("SELENIUM_HOST", "127.0.0.1")
+SELENIUM_PORT = os.environ.get("SELENIUM_PORT", "4444")
 
+session = create_session(DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME)
+today = datetime.date.today()
+for month in range(0, UPDATE_MONTHS):
+    d = today - relativedelta(months=month)
+    # TODO: カレンダー月になってしまっているので正しく営業月にする
+    fiscal_year = d.year
+    fiscal_month = d.month
+    # なんか月の切り替えにたまに失敗するので、月ごとにdriverをつくりなおす
+    with MoneyForwardScraper(
+        moneyforward_user=MONEYFORWARD_USER,
+        moneyforward_password=MONEYFORWARD_PASSWORD,
+        selenium_host=SELENIUM_HOST,
+        selenium_port=SELENIUM_PORT,
+        moneyforward_group_name=MONEYFORWARD_GROUP,
+    ) as mf:
+        # 入出金の更新
+        cashflows = mf.get_cashflows_of_fiscal_month(fiscal_year=fiscal_year, fiscal_month=fiscal_month)
+        for cashflow in cashflows:
+            c = Cashflow(
+                id=cashflow.id,
+                group=MONEYFORWARD_GROUP,
+                calc=cashflow.calc,
+                date=cashflow.date,
+                fiscal_year=cashflow.fiscal_year,
+                fiscal_month=cashflow.fiscal_month,
+                content=cashflow.content,
+                amount=cashflow.amount,
+                note=cashflow.note,
+                lcategory=cashflow.lcategory,
+                mcategory=cashflow.mcategory,
+                memo=cashflow.memo,
+            )
+            session.merge(c)
+            session.commit()
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("group", help='グループ名')
-    return parser.parse_args()
-
-
-if __name__ == '__main__':
-    args = parse_args()
-    group = args.group
-    with MoneyForwardScraper(MONEYFORWARD_USER, MONEYFORWARD_PASSWORD, group_name=group) as mf:
-        fiscal_year = 2024
-        for i in range(10, 13):
-            cashflows = mf.get_cashflows_of_fiscal_month(fiscal_year=fiscal_year, fiscal_month=i)
-            for cashflow in cashflows:
-                c = Cashflow(
-                    id=cashflow.id,
-                    calc=cashflow.calc,
-                    date=cashflow.date,
-                    fiscal_year=cashflow.fiscal_year,
-                    fiscal_month=cashflow.fiscal_month,
-                    content=cashflow.content,
-                    amount=cashflow.amount,
-                    note=cashflow.note,
-                    lcategory=cashflow.lcategory,
-                    mcategory=cashflow.mcategory,
-                    memo=cashflow.memo,
-                )
-                session.merge(c)
-                session.commit()
+# 予算の更新
+with MoneyForwardScraper(
+    moneyforward_user=MONEYFORWARD_USER,
+    moneyforward_password=MONEYFORWARD_PASSWORD,
+    selenium_host=SELENIUM_HOST,
+    selenium_port=SELENIUM_PORT,
+    moneyforward_group_name=MONEYFORWARD_GROUP,
+) as mf:
+    budgets = mf.get_budgets_of_group()
+    for budget in budgets:
+        b = Budget(
+            fiscal_year=today.year,
+            fiscal_month=today.month,
+            group=MONEYFORWARD_GROUP,
+            lcategory=budget.lcategory,
+            budget=budget.budget,
+        )
+        session.merge(b)
+        session.commit()
